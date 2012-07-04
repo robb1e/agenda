e23f535bddb75b2e943bdba0a70c4c3fc1c8a158
Robbie Clutton <robert.clutton@gmail.com>
2012-07-04 12:47:52 -0400

    Display current plans on logged in home page.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,4 +2,15 @@ require 'spec_helper'
 
 describe HomeController do
 
+  describe "#index" do
+    let(:user) { stub }
+    let(:plans) { stub }
+    it "assigs plans" do
+      @controller.should_receive(:current_user) { user }
+      user.should_receive(:plans) { plans }
+      get :index
+      assigns(:plans).should == plans
+    end
+  end
+
 end
