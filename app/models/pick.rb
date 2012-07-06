7fd27399d54f9ebe65231328e4c1ed9617de789e
Robbie Clutton <robert.clutton@gmail.com>
2012-07-05 23:07:48 -0400

    Mass assign plan_id

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -1,5 +1,5 @@
 class Pick < ActiveRecord::Base
-  attr_accessible :from, :until, :place, :plan
+  attr_accessible :from, :until, :place, :plan, :plan_id
   belongs_to :place
   belongs_to :plan
   
