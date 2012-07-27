7fc97b1050104511ea892e8a2b84e0845cc062fb
Robbie Clutton <robert.clutton@gmail.com>
2012-07-26 20:05:38 -0400

    use planner helper

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -1,12 +1,10 @@
 class PicksController < AuthenticatedController
 
   def show
-    planner = Agenda::Planner.new(params[:plan_id])
     @pick = planner.pick(params[:id])
   end
 
   def update
-    planner = Agenda::Planner.new(params[:plan_id])
     pick = planner.pick(params[:id])
     pick.update_attributes(params[:pick])
     redirect_to plan_path(planner.plan_id)
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,5 @@
 class PlacesController < AuthenticatedController
   def create
-    planner = Agenda::Planner.new(params[:plan_id])
     planner.add_venue_by_foursquare_id(params[:venue_id])
     redirect_to plan_path(planner.plan)
   end
